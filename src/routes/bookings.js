25569e4ed5f634ab0455b2058893e799a8931770
Eszter Mázi <mazieszter@gmail.com>
2025-12-10 16:19:39 +0100

    Add POST endpoint to create new bookings

--- src/routes/bookings.js
+++ src/routes/bookings.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { readJSON } from "../utils/storage.js";
+import { readJSON, writeJSON } from "../utils/storage.js";
+import { v4 as uuid } from "uuid";
 const router = express.Router()
 
 const BOOKINGS_PATH = "./src/data/bookings.json";
@@ -14,6 +15,28 @@ router.get("/", (req, res) => {
   }
 
   return res.json(result);
+});
+
+router.post("/", (req, res) => {
+  const { barberId, date, email } = req.body;
+
+  if (!barberId || !date || !email) {
+    return res.status(400).json({ error: "Missing required data" });
+  }
+
+  const bookings = readJSON(BOOKINGS_PATH);
+
+  const newBooking = {
+    id: uuid(),
+    barberId,
+    date,
+    email,
+  };
+
+  bookings.push(newBooking);
+  writeJSON(BOOKINGS_PATH, bookings);
+
+  return res.status(201).json(newBooking);
 });
 
 export default router;
